987d2381c6e6e406c07ba92a6e9c74f303d9b971
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-01-26 15:45:58 -0300

    chore(helper): validate email

--- helpers/htypes/email.go
+++ helpers/htypes/email.go
@@ -1,11 +1,32 @@
 package htypes
 
-import "github.com/kgjoner/cornucopia/helpers/validator"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/kgjoner/cornucopia/helpers/normalizederr"
+)
 
 type Email string
 
 func (e Email) IsValid() error {
-	return validator.Validate(string(e), "email")
+	str := string(e)
+	if str == "" {
+		return nil
+	}
+
+	doesMatch, err := regexp.MatchString(
+		`^[a-z0-9!#$%&'*+/=?^_`+"`{|}~-]+(?:"+`\.[a-z0-9!#$%&'*+/=?^_`+"`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"+`\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$`,
+		str,
+	)
+	if err != nil {
+		return err
+	} else if doesMatch {
+		return nil
+	}
+
+	msg := fmt.Sprintf("Must be a valid email.")
+	return normalizederr.NewValidationError(msg)
 }
 
 func (e Email) IsZero() bool {
