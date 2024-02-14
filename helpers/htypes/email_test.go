81c2129d6e51c8b6e0f4a30482ec927eed38a564
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-02-14 12:09:43 -0300

    test: add test for email htype

---
+++ helpers/htypes/email_test.go
@@ -0,0 +1,34 @@
+package htypes_test
+
+import (
+	"testing"
+
+	"github.com/kgjoner/cornucopia/helpers/htypes"
+	"github.com/kgjoner/cornucopia/helpers/validator"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmailValidation(t *testing.T) {
+	empty := htypes.Email("")
+	err := empty.IsValid()
+	assert.Nil(t, err)
+
+	invalid := htypes.Email("test.com")
+	err = invalid.IsValid()
+	assert.NotNil(t, err)
+
+	valid := htypes.Email("test@test.com")
+	err = valid.IsValid()
+	assert.Nil(t, err)
+
+	insideStr := struct {
+		Name string
+		Email htypes.Email `validate:"required"`
+	}{
+		"Dummy",
+		empty,
+	}
+	err = validator.Validate(insideStr)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "Email: Required")
+}
