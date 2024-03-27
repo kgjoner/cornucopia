58d94659f05063e1716f1509aa876d981f06c4f4
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-03-27 14:05:28 -0300

    feat: enhance document htype

--- helpers/htypes/document.go
+++ helpers/htypes/document.go
@@ -1,17 +1,44 @@
 package htypes
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/kgjoner/cornucopia/helpers/normalizederr"
 	"github.com/kgjoner/cornucopia/helpers/validator"
+	"github.com/kgjoner/cornucopia/utils/sanitizer"
+	"github.com/kgjoner/cornucopia/utils/sliceman"
 )
 
 type Document string
 
-func (d Document) IsValid() error {
-	parsed := d.Parse()
-	return validator.Validate(parsed)
-} 
+func ParseDocument(str string) (*Document, error) {
+	d := Document(str)
+	return &d, d.IsValid()
+}
+
+func (d *Document) IsValid() error {
+	if d.IsZero() {
+		return nil
+	}
+
+	formatted, err := d.Format()
+	if err != nil {
+		return err
+	}
+
+	switch formatted.Kind {
+	case "cpf":
+		return validateCpf(formatted.Number)
+	case "cnpj":
+		return validateCnpj(formatted.Number)
+	case "passport":
+		return validatePassport(formatted.Number)
+	default:
+		return normalizederr.NewValidationError("Not accepted document kind.")
+	}
+}
 
 func (d Document) IsZero() bool {
 	return d == ""
@@ -21,19 +48,167 @@ func (d Document) String() string {
 	return string(d)
 }
 
-type ParsedDocument struct {
-	Number string `validate:"required"`
+type FormattedDocument struct {
 	Kind   string `validate:"required,oneof=cpf cnpj passport"`
+	Number string `validate:"required"`
 }
 
-func (d Document) Parse() ParsedDocument {
-	parts := strings.Split(string(d), "_")
+func (d *Document) Format() (*FormattedDocument, error) {
+	parts := strings.Split(d.String(), ":")
 	if len(parts) != 2 {
-		return ParsedDocument{}
+		parts = make([]string, 2)
+		parts[1] = sanitizer.Digit(d.String())
+		switch len(parts[1]) {
+		case 11:
+			parts[0] = "cpf"
+		case 14:
+			parts[0] = "cnpj"
+		default:
+			return nil, normalizederr.NewValidationError("Unrecognizable document. Try to inform its type in the form {type}:{number}.")
+		}
+
+		*d = Document(fmt.Sprintf("%v:%v", parts[0], parts[1]))
+
+	} else if parts[0] != "passport" {
+		parts[1] = sanitizer.Digit(parts[1])
+		*d = Document(fmt.Sprintf("%v:%v", parts[0], parts[1]))
 	}
 
-	return ParsedDocument {
+	res := &FormattedDocument{
+		Kind:   parts[0],
 		Number: parts[1],
-		Kind: parts[0],
 	}
+	return res, validator.Validate(res)
+}
+
+func validateCpf(cpf string) error {
+	if len(cpf) != 11 {
+		return normalizederr.NewValidationError("Invalid CPF")
+	}
+
+	// Exclude invalid numbers
+	invalidNumbers := []string{
+		"00000000000",
+		"11111111111",
+		"22222222222",
+		"33333333333",
+		"44444444444",
+		"55555555555",
+		"66666666666",
+		"77777777777",
+		"88888888888",
+		"99999999999",
+	}
+	if sliceman.IndexOf(invalidNumbers, cpf) != -1 {
+		return normalizederr.NewValidationError("Invalid CPF")
+	}
+
+	// First verification digit
+	add := 0
+	for i := 0; i < 9; i++ {
+		digit, _ := strconv.Atoi(string(cpf[i]))
+		add += (10 - i) * digit
+	}
+
+	rev := 11 - (add % 11)
+	if rev == 10 || rev == 11 {
+		rev = 0
+	}
+
+	verDig, _ := strconv.Atoi(string(cpf[9]))
+	if rev != verDig {
+		return normalizederr.NewValidationError("Invalid CPF")
+	}
+
+	// Second verification digit
+	add = 0
+	for i := 0; i < 10; i++ {
+		digit, _ := strconv.Atoi(string(cpf[i]))
+		add += (11 - i) * digit
+	}
+
+	rev = 11 - (add % 11)
+	if rev == 10 || rev == 11 {
+		rev = 0
+	}
+
+	verDig, _ = strconv.Atoi(string(cpf[10]))
+	if rev != verDig {
+		return normalizederr.NewValidationError("Invalid CPF")
+	}
+
+	return nil
+}
+
+func validateCnpj(cnpj string) error {
+	if len(cnpj) != 14 {
+		return normalizederr.NewValidationError("Invalid CNPJ")
+	}
+
+	// Exclude invalid numbers
+	invalidNumbers := []string{
+		"00000000000000",
+		"11111111111111",
+		"22222222222222",
+		"33333333333333",
+		"44444444444444",
+		"55555555555555",
+		"66666666666666",
+		"77777777777777",
+		"88888888888888",
+		"99999999999999",
+	}
+	if sliceman.IndexOf(invalidNumbers, cnpj) != -1 {
+		return normalizederr.NewValidationError("Invalid CNPJ")
+	}
+
+	// First verification digit
+	add := 0
+	for i := 0; i < 12; i++ {
+		multiplier := 13 - i
+		if i < 4 {
+			multiplier = 5 - i
+		}
+
+		digit, _ := strconv.Atoi(string(cnpj[i]))
+		add += multiplier * digit
+	}
+
+	rev := 11 - (add % 11)
+	if rev == 10 || rev == 11 {
+		rev = 0
+	}
+
+	verDig, _ := strconv.Atoi(string(cnpj[12]))
+	if rev != verDig {
+		return normalizederr.NewValidationError("Invalid CNPJ")
+	}
+
+	// Second verification digit
+	add = 0
+	for i := 0; i < 13; i++ {
+		multiplier := 14 - i
+		if i < 5 {
+			multiplier = 6 - i
+		}
+
+		digit, _ := strconv.Atoi(string(cnpj[i]))
+		add += multiplier * digit
+	}
+
+	rev = 11 - (add % 11)
+	if rev == 10 || rev == 11 {
+		rev = 0
+	}
+
+	verDig, _ = strconv.Atoi(string(cnpj[13]))
+	if rev != verDig {
+		return normalizederr.NewValidationError("Invalid CNPJ")
+	}
+
+	return nil
+}
+
+func validatePassport(_ string) error {
+	return nil
 }
---
+++ helpers/htypes/document_test.go
@@ -0,0 +1,31 @@
+package htypes_test
+
+import (
+	"testing"
+
+	"github.com/kgjoner/cornucopia/helpers/htypes"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDocumentParsing(t *testing.T) {
+	// Simple CPF
+	unformatted := "024.969.460-31"
+	doc, err := htypes.ParseDocument(unformatted)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "cpf:02496946031", doc.String())
+	
+	// Simple CNPJ
+	unformatted = "86.978.987/0001-39"
+	doc, err = htypes.ParseDocument(unformatted)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "cnpj:86978987000139", doc.String())
+	
+	// Empty
+	unformatted = ""
+	doc, err = htypes.ParseDocument(unformatted)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "", doc.String())
+}
