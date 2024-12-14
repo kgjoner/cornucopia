b30772f7f586fe9b7c41538df36c267c0cbd82b9
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2024-12-14 19:40:35 -0300

    fix(helper): add driver interfaces to null time

--- helpers/htypes/null.go
+++ helpers/htypes/null.go
@@ -20,6 +20,28 @@ func (t *NullTime) MarshalJSON() ([]byte, error) {
 	return t.Time.MarshalJSON()
 }
 
+func (t *NullTime) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+
+	switch src := src.(type) {
+	case time.Time:
+		t.Time = src
+	default:
+		return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type %T", src, []byte{})
+	}
+	return nil
+}
+
+// Value implements the driver.Valuer interface.
+func (t NullTime) Value() (driver.Value, error) {
+	if t.IsZero() {
+		return nil, nil
+	}
+	return t.Time, nil
+}
+
 // RAW MESSAGE
 type NullRawMessage struct {
 	RawMessage json.RawMessage
@@ -42,7 +64,7 @@ func (n *NullRawMessage) Scan(src interface{}) error {
 	return nil
 }
 
-// Value implements the driver Valuer interface.
+// Value implements the driver.Valuer interface.
 func (n NullRawMessage) Value() (driver.Value, error) {
 	if !n.Valid {
 		return nil, nil
