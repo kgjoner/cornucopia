8e33cb41084ea187043418cdd2c52eae595d38e7
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2025-10-06 10:01:45 -0300

    feat(pwdgen)!: ensure secure generation and add numeric code

--- utils/pwdgen/pwdgen.go
+++ utils/pwdgen/pwdgen.go
@@ -1,61 +1,90 @@
 package pwdgen
 
 import (
-	"math/rand"
+	cryptorand "crypto/rand"
+	"math/big"
 	"strings"
 
 	"github.com/kgjoner/cornucopia/utils/sliceman"
 )
 
-// Generate a string with desired length including random runes based on selected sets. The set options are: lower, upper, number and special. It will include at least one rune of each set. If no set is provided, it will include all of them. The minimum length is equal to the variety of selected sets, i.e., 4 if all of them are selected.
-func Generate(length int, sets ...string) string {
+// GeneratePassword creates a string with desired length including random runes based
+// on selected sets. The set options are: lower, upper, number and special. It will
+// include at least one rune of each set. If no set is provided, it will include all
+// of them. The minimum length is equal to the variety of selected sets, i.e., 4 if
+// all of them are selected.
+func GeneratePassword(length int, sets ...string) string {
 	var password strings.Builder
 
 	if len(sets) == 0 {
 		sets = []string{"lower", "upper", "number", "special"}
 	}
 
-	fullSet := "";
-	if (sliceman.IndexOf(sets, "lower") != -1) {
-		lowerCharSet := "abcdedfghijklmnopqrst"
+	fullSet := ""
+	if sliceman.IndexOf(sets, "lower") != -1 {
+		lowerCharSet := "abcdefghijklmnopqrstuvwxyz"
 		fullSet += lowerCharSet
 
-		index := rand.Intn(len(lowerCharSet))
+		index := secureRandomInt(len(lowerCharSet))
 		password.WriteString(string(lowerCharSet[index]))
 	}
 
-	if (sliceman.IndexOf(sets, "upper") != -1) {
+	if sliceman.IndexOf(sets, "upper") != -1 {
 		upperCharSet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		fullSet += upperCharSet
 
-		index := rand.Intn(len(upperCharSet))
+		index := secureRandomInt(len(upperCharSet))
 		password.WriteString(string(upperCharSet[index]))
 	}
 
-	if (sliceman.IndexOf(sets, "number") != -1) {
+	if sliceman.IndexOf(sets, "number") != -1 {
 		numberSet := "0123456789"
 		fullSet += numberSet
 
-		index := rand.Intn(len(numberSet))
+		index := secureRandomInt(len(numberSet))
 		password.WriteString(string(numberSet[index]))
 	}
 
-	if (sliceman.IndexOf(sets, "special") != -1) {
+	if sliceman.IndexOf(sets, "special") != -1 {
 		specialCharSet := "!@#$%&*"
 		fullSet += specialCharSet
 
-		index := rand.Intn(len(specialCharSet))
+		index := secureRandomInt(len(specialCharSet))
 		password.WriteString(string(specialCharSet[index]))
 	}
 
 	for i := 0; i < (length - len(sets)); i++ {
-		index := rand.Intn(len(fullSet))
+		index := secureRandomInt(len(fullSet))
 		password.WriteString(string(fullSet[index]))
 	}
 
 	inRune := []rune(password.String())
-	rand.Shuffle(len(inRune), func(i, j int) {
+	for i := len(inRune) - 1; i > 0; i-- {
+		j := secureRandomInt(i + 1)
 		inRune[i], inRune[j] = inRune[j], inRune[i]
-	})
+	}
+
 	return string(inRune)
+}
+
+// GenerateNumericCode creates a secure numeric code of the specified length.
+func GenerateNumericCode(length int) string {
+	const numberSet = "0123456789"
+
+	code := make([]byte, length)
+	for i := 0; i < length; i++ {
+		index := secureRandomInt(len(numberSet))
+		code[i] = numberSet[index]
+	}
+
+	return string(code)
+}
+
+// secureRandomInt generates a cryptographically secure random integer
+func secureRandomInt(max int) int {
+	n, err := cryptorand.Int(cryptorand.Reader, big.NewInt(int64(max)))
+	if err != nil {
+		panic("Failed to generate secure random number")
+	}
+	return int(n.Int64())
 }
